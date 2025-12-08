369dcdcc019e6181d91ed02f7c57be9564c25d92
omoefe <raniem57@gmail.com>
2025-12-08 21:27:35 +0100

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,11 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com", // For Google profile pics (if you ever use Google login)
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // ✅ allow Cloudinary
       },
     ],
   },
